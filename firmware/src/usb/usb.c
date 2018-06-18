ab0d35c4fe061a402d49637eeeec5948006288b3
Alex Petrov <a.y.p@inbox.ru>
2018-06-18 20:56:34 +0600

    slightly modified _read func

--- firmware/src/usb/usb.c
+++ firmware/src/usb/usb.c
@@ -35,9 +35,10 @@ int _write(int file, char *ptr, int len)
 
 int _read (int file, char *ptr, int len)
 {
-	if (file == 0)
+	if (file == 0) {
 		return usbrw_read_nonblock (_stdout) ? usbrw_read (_stdout, ptr, len) : 0;	
-
+	}
+	
 	errno = EIO;
 	return -1;
 }
