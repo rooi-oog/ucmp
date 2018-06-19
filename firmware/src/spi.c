86517ff83238e02ea67f7d12b8800f001db05e9e
Alex Petrov <a.y.p@inbox.ru>
2018-06-19 20:38:54 +0600

    remove some warnings

--- firmware/src/spi.c
+++ firmware/src/spi.c
@@ -8,17 +8,20 @@
 
 bool spi_write_byte (uint32_t address, uint8_t data) 
 {
-	
+	(void) address;
+	(void) data;
 }
 
 bool spi_write_page (uint32_t address, uint8_t *data_ptr, uint16_t data_length) 
 {
-	
+	(void) address;
+	(void) data_ptr;
 }
 
 bool spi_read_byte (uint32_t address, uint8_t *byte_ptr) 
 {
-	
+	(void) address;
+	(void) byte_ptr
 }
 
 bool spi_read_page (uint32_t address, uint8_t *byte_ptr, uint16_t data_length)
